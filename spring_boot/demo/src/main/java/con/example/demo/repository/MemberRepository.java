78cffedbf2593eb5fd8e32b26abd6a68f1a3916f
SeokHyeon Kim <6017891@naver.com>
2022-10-01 22:28:56 +0900

    add bootEx1

---
+++ spring_boot/demo/src/main/java/con/example/demo/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package con.example.demo.repository;
+
+import con.example.demo.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> FindByName(String name);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+
+
+
+}
